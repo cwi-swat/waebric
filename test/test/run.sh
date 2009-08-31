0d588a9e4dbb06dfd532857fc4c6b1248f2d7920
Tijs van der Storm <T.van.der.Storm@cwi.nl>
2009-08-31 13:52:49 +0000

    Added AST results.

--- test/test/run.sh
+++ test/test/run.sh
@@ -4,9 +4,11 @@ OUTPUT=output
 
 for t in $TESTS; do
     out=${OUTPUT}/`basename $t .wae`.html
+    ast=${OUTPUT}/`basename $t .wae`.ast
     err=${OUTPUT}/`basename $t .wae`.err
     echo "Running $t"
     waebric $t &>$err  > $out
+    waebric-ast $t > $ast
 done
 
 
